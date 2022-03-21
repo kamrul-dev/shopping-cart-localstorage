c16293282619f0f5bbbaeffbdab701be09af0190
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-21 16:10:07 +0600

    styles added in dive

--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
+import './Cosmetics.css'
 
 const Cosmetics = () => {
     const [cosmetics, setCosmetics] = useState([])
     console.log(cosmetics)
     useEffect(() => {
         fetch('data.json')
-        .then(res => res.json())
-        .then(data => setCosmetics(data));
+            .then(res => res.json())
+            .then(data => setCosmetics(data));
     }, []);
     return (
         <div>
             <h1>Welcome to Cosmetics Shop</h1>
-            {
-                cosmetics.map(cosmetic => <Cosmetic
-                    key={cosmetic.id}
-                    cosmetic={cosmetic}
-                ></Cosmetic>)
-            }
+            <div className='products'>
+                {
+                    cosmetics.map(cosmetic => <Cosmetic
+                        key={cosmetic.id}
+                        cosmetic={cosmetic}
+                    ></Cosmetic>)
+                }
+            </div>
         </div>
     );
 };

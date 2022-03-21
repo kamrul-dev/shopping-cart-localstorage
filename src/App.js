dadd8a2102657bb5e7482d10ff59b6fa89902b0e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-21 15:04:03 +0600

    load the data and display on UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Cosmetics from './Components/Cosmetics/Cosmetics';
 
 function App() {
   return (
     <div className="App">
-      
+      <Cosmetics></Cosmetics>
     </div>
   );
 }
---
+++ src/Components/Cosmetic/Cosmetic.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Cosmetic.css'
+
+const Cosmetic = (props) => {
+    const {name, price, id} = props.cosmetic;
+    return (
+        <div className='product'>
+            <h3>name: {name}</h3>
+            <h4>Price: {price}</h4>
+            <p>It has id: {id}</p>
+        </div>
+    );
+};
+
+export default Cosmetic;
